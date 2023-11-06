0cc452c07018ca5f81abb85e59060aec936e07d0
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-06 13:06:45 +0300

    Time: 173 ms (5.08%), Space: 63.9 MB (15.89%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.java
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.java
@@ -1,8 +1,23 @@
-
-
 import java.util.ArrayList;
 import java.util.List;
 
+
+//class ListNode {
+//    int val;
+//    ListNode next;
+//
+//    ListNode() {
+//    }
+//
+//    ListNode(int val) {
+//        this.val = val;
+//    }
+//
+//    ListNode(int val, ListNode next) {
+//        this.val = val;
+//        this.next = next;
+//    }
+//}
 
 public class Solution {
 
@@ -12,7 +27,7 @@ public class Solution {
     }
 
     private void start() {
-        //test();
+        test();
         test1();
     }
 
