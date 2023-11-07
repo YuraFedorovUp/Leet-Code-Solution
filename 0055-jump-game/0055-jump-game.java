641ef345b5514a3936c0dbf656b36435cceb5d2e
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-07 15:59:45 +0300

    Time: 285 ms (11.32%), Space: 43.6 MB (94.24%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public static void main(String[] args) {
+        Solution main = new Solution();
+        main.start();
+    }
+
+    private void start() {
+        test1();
+    }
+
+    private void test1() {
+        System.out.println(canJump(new int[]{3, 2, 1, 0, 4}));
+        System.out.println(canJump(new int[]{0}));
+        System.out.println(canJump(new int[]{2, 3, 1, 1, 4}));
+    }
+
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+        final boolean[] opportunityJumps = new boolean[nums.length];
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (!opportunityJumps[i] && i != 0) {
+                continue;
+            }
+            insertJump(i, opportunityJumps, nums);
+        }
+        return opportunityJumps[nums.length - 1];
+    }
+
+    private void insertJump(int index, boolean[] opportunityJumps, int[] nums) {
+        final int jumpLength = nums[index];
+        for (int i = index + 1; i < index + 1 + jumpLength && i < nums.length; i++) {
+            opportunityJumps[i] = true;
+        }
+    }
+}
