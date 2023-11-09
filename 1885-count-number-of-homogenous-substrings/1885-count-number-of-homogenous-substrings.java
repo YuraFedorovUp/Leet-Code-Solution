f3553899f6eeb88b00a1018c98c796d75b8a36da
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-09 11:33:48 +0300

    Time: 11 ms (41.2%), Space: 44.1 MB (57.2%) - LeetHub

---
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.java
@@ -0,0 +1,32 @@
+class Solution {
+    private static final int MODULO = 1_000_000_007;
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.start();
+
+    }
+
+    private void start() {
+        test();
+    }
+
+    private void test() {
+        System.out.println(countHomogenous("abbcccaa"));
+    }
+
+    public int countHomogenous(String s) {
+        int left = 0;
+        int right = 0;
+        int result = 0;
+        while (right < s.length()) {
+            if (s.charAt(left) != s.charAt(right)) {
+                left = right;
+            }
+            result += right - left + 1;
+            result %= MODULO;
+            right++;
+        }
+        return result;
+    }
+}
