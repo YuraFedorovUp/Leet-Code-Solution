2d59ba20ccf0769e33bd194bf7ca13534031925e
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-09 21:01:36 +0300

    Time: 2 ms (11.18%), Space: 40.2 MB (90.38%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+ public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        final int m = obstacleGrid.length;
+        final int n = obstacleGrid[0].length;
+        if (obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+           if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    obstacleGrid[i][j] = -1;
+                }
+            }
+        }
+        obstacleGrid[0][0] = 1;
+        final Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(0);
+        queue.add(0);
+        while (queue.size() != 0) {
+            int y = queue.remove();
+            int x = queue.remove();
+            addStep(y + 1, x, queue, obstacleGrid, m, n, obstacleGrid[y][x]);
+            addStep(y, x + 1, queue, obstacleGrid, m, n, obstacleGrid[y][x]);
+        }        
+        int result = obstacleGrid[m - 1][n - 1];
+        return result;
+    }
+
+    private void addStep(int y, int x, Queue<Integer> queue, int[][] obstacleGrid, int m, int n, int mean) {
+        if (y >= m || y < 0 || x >= n || x < 0) {
+            return;
+        }
+        if (obstacleGrid[y][x] == -1) {
+           return;
+        }
+        if (obstacleGrid[y][x] == 0) {
+            queue.add(y);
+            queue.add(x);
+        }
+        obstacleGrid[y][x] += mean;
+    }
+}
