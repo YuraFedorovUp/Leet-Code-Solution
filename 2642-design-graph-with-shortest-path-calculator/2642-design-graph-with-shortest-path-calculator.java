99d2b468b55d669a4dfbdc00af8c6392cf24f0ca
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-11 23:38:04 +0300

    Time: 141 ms (50.48%), Space: 55 MB (18.10%) - LeetHub

--- 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.java
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.java
@@ -1,48 +1,103 @@
-class Graph {
-    List<List<Pair<Integer, Integer>>> adjList;
-    public Graph(int n, int[][] edges) {
-        adjList = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            adjList.add(new ArrayList<>());
-        for (int[] e : edges)
-            adjList.get(e[0]).add(new Pair<>(e[1], e[2]));
-    }
+  class Graph {
+        private final Map<Integer, List<Edge>> numberToEdges;
+        private final int size;
+        private boolean[] used = new boolean[1];
 
-    public void addEdge(int[] edge) {
-        adjList.get(edge[0]).add(new Pair<>(edge[1], edge[2]));
-    }
+        public Graph(int n, int[][] edges) {
+            numberToEdges = new HashMap<>();
+            size = n;
+            for (int i = 0; i < edges.length; i++) {
+                addEdge(edges[i]);
 
-    public int shortestPath(int node1, int node2) {
-        int n = adjList.size();
-        var pq = new PriorityQueue<List<Integer>>(Comparator.comparingInt(list -> list.get(0)));
-        int[] costForNode = new int[n];
-        Arrays.fill(costForNode, Integer.MAX_VALUE);
-        costForNode[node1] = 0;
-        pq.offer(Arrays.asList(0, node1));
-
-        while (!pq.isEmpty()) {
-            var curr = pq.poll();
-            int currCost = curr.get(0);
-            int currNode = curr.get(1);
-
-            if (currCost > costForNode[currNode]) {
-                continue;
-            }
-            if (currNode == node2) {
-                return currCost;
-            }
-            for (var neighbor : adjList.get(currNode)) {
-                int neighborNode = neighbor.getKey();
-                int cost = neighbor.getValue();
-                int newCost = currCost + cost;
-
-                if (newCost < costForNode[neighborNode]) {
-                    costForNode[neighborNode] = newCost;
-                    pq.offer(Arrays.asList(newCost, neighborNode));
-                }
             }
         }
 
-        return -1;
+        public void addEdge(int[] edge) {
+            final int number = edge[0];
+            final Edge nextEdg = new Edge(edge[1], edge[2]);
+            List<Edge> edges1 = numberToEdges.getOrDefault(number, new ArrayList<>());
+            edges1.add(nextEdg);
+            numberToEdges.put(number, edges1);
+        }
+
+        public int shortestPath(int node1, int node2) {
+            final PriorityQueue<Step> queue = new PriorityQueue<>();
+            used = new boolean[size];
+            if (node1 == node2) {
+                return 0;
+            }
+            queue.add(new Step(node1, 0));
+            int result = Integer.MAX_VALUE;
+            while (queue.size() != 0) {
+                final Step step = queue.remove();
+                if (used[step.getEdge()]) {
+                    continue;
+                }
+                used[step.getEdge()] = true;
+                if (step.getEdge() == node2) {
+                    return step.getCurrentCoast();
+                }
+                final List<Edge> edges = numberToEdges.get(step.getEdge());
+                if (edges == null) {
+                    continue;
+                }
+                final int currentCoast = step.getCurrentCoast();
+                for (Edge edge : edges) {
+                    if (currentCoast + edge.getCoast() >= result) {
+                        continue;
+                    }
+                    queue.add(new Step(edge.getNextEdge(), edge.getCoast() + currentCoast));
+                }
+            }
+            return -1;
+        }
     }
-}
+
+    class Step implements Comparable<Step> {
+        private int edge;
+        private int currentCoast;
+
+        public Step(int edge, int currentCoast) {
+            this.edge = edge;
+            this.currentCoast = currentCoast;
+        }
+
+        public int getCurrentCoast() {
+            return currentCoast;
+        }
+
+        public int getEdge() {
+            return edge;
+        }
+
+        @Override
+        public int compareTo(Step o) {
+            return this.getCurrentCoast() - o.getCurrentCoast();
+        }
+
+        @Override
+        public String toString() {
+            return "Step{" +
+                    "edge=" + edge +
+                    ", currentCoast=" + currentCoast +
+                    '}';
+        }
+    }
+
+    class Edge {
+        private int nextEdge;
+        private int coast;
+
+        public Edge(int nextEdge, int coast) {
+            this.nextEdge = nextEdge;
+            this.coast = coast;
+        }
+
+        public int getNextEdge() {
+            return nextEdge;
+        }
+
+        public int getCoast() {
+            return coast;
+        }
+    }
