e201782c17edd6e82aa233a0551c7481dfec6608
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-18 00:07:16 +0300

    Time: 52 ms (93.43%), Space: 57.4 MB (23.24%) - LeetHub

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution main = new Solution();
+        main.start();
+    }
+
+    private void start() {
+        test();
+    }
+
+    private void test() {
+        int heights[] = new int[]{3, 5, 4, 2, 4, 6};
+        System.out.println(minPairSum(heights));
+    }
+
+    private void printMy(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+        System.out.println();
+    }
+
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = 0;
+        while (left < right) {
+            int cur = nums[left] + nums[right];
+            if (cur > ans) {
+                ans = cur;
+            }
+            left++;
+            right--;
+        }
+        return ans;
+    }
+}
