e2b460f96901dbd9a8291af3b44359ebc51eced7
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-23 21:43:10 +0300

    Time: 13 ms (73.96%), Space: 43.7 MB (84.74%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -3,6 +3,81 @@ import java.util.Stack;
 
 public class Solution {
 
+    public static void main(String[] args) {
+        Solution main = new Solution();
+        main.start();
+    }
+
+    private void start() {
+        test();
+        test1();
+        test2();
+        test3();
+        test4();
+        test5();
+        test6();
+        test10();
+        test11();
+        test12();
+        test13();
+    }
+
+    private void test() {
+        final int nums[] = new int[]{1, 2, 2, 2, 3};
+        System.out.println(largestRectangleArea(nums));
+    }
+
+    private void test1() {
+        final int nums[] = new int[]{1, 3, 3, 3, 5, 2};
+        System.out.println(largestRectangleArea(nums));
+    }
+
+    private void test2() {
+        final int nums[] = new int[]{1, 2, 1, 0, 1, 2, 1, 1};
+        System.out.println(largestRectangleArea(nums));
+    }
+
+    private void test3() {
+        final int nums[] = new int[]{1, 2, 1, 0};
+        System.out.println(largestRectangleArea(nums));
+    }
+
+    private void test4() {
+        final int nums[] = new int[]{2, 1, 5, 6, 2, 3};
+        System.out.println(largestRectangleArea(nums));
+    }
+
+    private void test5() {
+        final int nums[] = new int[]{5, 6, 2, 3};
+        System.out.println(largestRectangleArea(nums));
+    }
+
+    private void test6() {
+        final int nums[] = new int[]{2, 1, 2};
+        System.out.println(largestRectangleArea(nums));
+    }
+
+    private void test10() {
+        final char nums[][] = new char[][]{{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
+        System.out.println(maximalRectangle(nums));
+    }
+
+    private void test11() {
+        final char nums[][] = new char[][]{{'1'}};
+        System.out.println(maximalRectangle(nums));
+    }
+
+
+    private void test12() {
+        final char nums[][] = new char[][]{{'0'}};
+        System.out.println(maximalRectangle(nums));
+    }
+
+    private void test13() {
+        final char nums[][] = new char[][]{{'1', '0'}, {'1', '0'}};
+        System.out.println(maximalRectangle(nums));
+    }
+
     public int maximalRectangle(char[][] matrix) {
         int ans = 0;
         int[] heights = new int[matrix[0].length];
