1b6a90854cd8a091806cb9d2c8cf27a7fbb35929
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-26 15:18:42 +0300

    Time: 105 ms (27.13%), Space: 85.7 MB (88.12%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -3,33 +3,30 @@ import java.util.Iterator;
 import java.util.Set;
 
 
+class RandomizedSet {
+    private Set<Integer> set;
 
+    public RandomizedSet() {
+        set = new HashSet();
+    }
 
-    class RandomizedSet {
-        private Set<Integer> set;
+    public boolean insert(int val) {
+        return set.add(val);
+    }
 
-        public RandomizedSet() {
-            set = new HashSet();
+    public boolean remove(int val) {
+        return set.remove(val);
+    }
+
+    public int getRandom() {
+        final int size = set.size();
+        int i = (int) (Math.random() * size) + 1;
+        Iterator iterator = set.iterator();
+        int ans = 0;
+        while (i != 0) {
+            ans = (Integer) iterator.next();
+            i--;
         }
-
-        public boolean insert(int val) {
-            return set.add(val);
-        }
-
-        public boolean remove(int val) {
-            return set.remove(val);
-        }
-
-        public int getRandom() {
-            final int size = set.size();
-            int i = (int) (Math.random() * size) + 1;
-            Iterator iterator = set.iterator();
-            int ans = 0;
-            while (i != 0) {
-                ans = (Integer) iterator.next();
-                i--;
-            }
-            return ans;
-        }
-    
+        return ans;
+    }
 }
