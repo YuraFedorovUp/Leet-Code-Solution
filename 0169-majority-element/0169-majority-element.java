ff3b2628ae497cd5de7c9b6353f1e5de7dda8d72
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-26 12:34:32 +0300

    Time: 11 ms (34.52%), Space: 46.7 MB (84.37%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,35 @@
-class Solution {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution main = new Solution();
+        main.start();
+    }
+
+    private void start() {
+        test();
+    }
+
+    private void test() {
+        int nums[] = new int[]{3, 2, 3};
+        System.out.println(majorityElement(nums));
+    }
+
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        final Map<Integer, Integer> numberToQuantity = new HashMap<>();
+        for (int number : nums) {
+            numberToQuantity.put(number, numberToQuantity.getOrDefault(number, 0) + 1);
+        }
+        int number = 0;
+        int maxQuantity = 0;
+        for (Map.Entry<Integer, Integer> entry : numberToQuantity.entrySet()) {
+            if (maxQuantity < entry.getValue()) {
+                maxQuantity = entry.getValue();
+                number = entry.getKey();
+            }
+        }
+        return number;
     }
 }
