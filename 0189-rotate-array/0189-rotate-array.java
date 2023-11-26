f9da3e362b757bfd097ef9d2b69a888780497a74
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-26 13:56:28 +0300

    Time: 2 ms (24.49%), Space: 56.2 MB (19.98%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,13 +1,49 @@
-class Solution {
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution main = new Solution();
+        main.start();
+    }
+
+    private void start() {
+        //   test();
+        test1();
+    }
+
+    private void test() {
+        int nums0[] = new int[]{1, 2, 3, 4, 5};
+        rotate(nums0, 2);
+        System.out.println(Arrays.toString(nums0));
+    }
+
+    private void test1() {
+        int nums0[] = new int[]{-1};
+        rotate(nums0, 2);
+        System.out.println(Arrays.toString(nums0));
+    }
+
     public void rotate(int[] nums, int k) {
         final int length = nums.length;
+        if (length == 1) {
+            return;
+        }
         final int threshold = k % length;
-        int clone[] = nums.clone();
-        for (int i = 0; i < threshold; i++) {
-            nums[i] = clone[length  - threshold+i];
+        reverse(0, length - 1, nums);
+        reverse(0, threshold - 1, nums);
+        reverse(threshold, length - 1, nums);
+    }
+
+    private void reverse(int left, int right, int[] nums) {
+        while (left < right) {
+            swap(left++, right--, nums);
         }
-        for (int i = 0; i < length - threshold ; i++) {
-            nums[i + threshold] = clone[i];
-        }
+    }
+
+    private void swap(int left, int right, int[] nums) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
