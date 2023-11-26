0674f601dfd462e8357e5e6869fad04aa185cb44
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-26 15:18:05 +0300

    Time: 98 ms (29.85%), Space: 85.8 MB (87.45%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
+
+
+    class RandomizedSet {
+        private Set<Integer> set;
+
+        public RandomizedSet() {
+            set = new HashSet();
+        }
+
+        public boolean insert(int val) {
+            return set.add(val);
+        }
+
+        public boolean remove(int val) {
+            return set.remove(val);
+        }
+
+        public int getRandom() {
+            final int size = set.size();
+            int i = (int) (Math.random() * size) + 1;
+            Iterator iterator = set.iterator();
+            int ans = 0;
+            while (i != 0) {
+                ans = (Integer) iterator.next();
+                i--;
+            }
+            return ans;
+        }
+    
+}
