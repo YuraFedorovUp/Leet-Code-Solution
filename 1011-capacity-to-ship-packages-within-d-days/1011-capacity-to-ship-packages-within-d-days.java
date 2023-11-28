3e554349b3b3de56f7b356627f9ab1cec0ceaa52
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-28 10:36:17 +0300

    Time: 10 ms (76.39%), Space: 45.3 MB (36.40%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,9 +1,6 @@
-
-import java.util.Map;
-import java.util.Stack;
-
 public class Solution {
-    int result = 0;
+    private int maxWeight = 0;
+    private int sumWeight = 0;
 
     public static void main(String[] args) {
         Solution main = new Solution();
@@ -11,93 +8,61 @@ public class Solution {
     }
 
     private void start() {
-        // test();
-        test1();
+        test();
     }
 
     private void test() {
-        System.out.println((shipWithinDays(new int[]{3, 2, 2, 4, 1, 4}, 3)));
-    }
-
-    private void test1() {
-        System.out.println((shipWithinDays(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5)));
+        System.out.println(shipWithinDays(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5));
     }
 
     public int shipWithinDays(int[] weights, int days) {
-        final CargoParameter cargoParameter = getCargoParameters(weights);
-        int capacity = getCapacity(cargoParameter, days, weights.length);
-        if (days == 1) {
-            return cargoParameter.sumWeight;
+        fillMaxWeightSumWeight(weights);
+        return binarySearch(weights, maxWeight, sumWeight, days);
+    }
+
+    private int binarySearch(int[] weights, int maxWeight, int sumWeight, int days) {
+        int left = maxWeight;
+        int right = sumWeight;
+        int result = Integer.MAX_VALUE;
+        while (left <= right) {
+            if (left == right && isApproachCapacity(weights, days, left)) {
+                result = Math.min(result, left);
+            }
+            int mid = left + (right - left) / 2;
+            final boolean isApproachCapacity = isApproachCapacity(weights, days, mid);
+            if (isApproachCapacity) {
+                result = Math.min(result, mid);
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-        fillResult(0, days, weights, 0, capacity);
         return result;
     }
 
-    private boolean fillResult(int currentDay, int days, int[] weights, int index, int capacity) {
-        if (currentDay == days) {
-            if (index == weights.length) {
-                result = capacity;
-                return true;
-            }
-            return false;
-        }
-        int curWeight = 0;
-        for (int i = index; i < weights.length; i++) {
-            curWeight += weights[i];
-            if (i != weights.length - 1) {
-                if (capacity >= curWeight + weights[i + 1]) {
-                    continue;
-                }
-                boolean ans = fillResult(currentDay + 1, days, weights, i + 1, capacity);
-                if (ans) {
-                    return true;
-                }
-                if (currentDay == 0) {
-                    while (!fillResult(0, days, weights, 0, capacity + 1)) ;
-                    return true;
-                }
-            } else {
-                if (capacity >= curWeight) {
-                    result = capacity;
-                    return true;
-                }
-                return false;
-            }
-            return false;
-        }
-        return false;
-    }
-
-    private int getCapacity(CargoParameter cargoParameter, int days, int count) {
-        final int sumWeight = cargoParameter.sumWeight;
-        final int maxWeight = cargoParameter.maxWeight;
-        final int averageWeight = cargoParameter.sumWeight / count;
-        if (maxWeight > averageWeight - 1) {
-            return maxWeight;
-        }
-        int capacity = sumWeight / days;
-        return capacity > 1 ? capacity : 1;
-    }
-
-    private CargoParameter getCargoParameters(int[] weights) {
-        int sumWeight = 0;
-        int maxWeight = 0;
+    private void fillMaxWeightSumWeight(int[] weights) {
+        maxWeight = 0;
+        sumWeight = 0;
         for (int weight : weights) {
             sumWeight += weight;
             if (weight > maxWeight) {
                 maxWeight = weight;
             }
         }
-        return new CargoParameter(sumWeight, maxWeight);
     }
 
-    class CargoParameter {
-        int sumWeight;
-        int maxWeight;
 
-        public CargoParameter(int sumWeight, int maxWeight) {
-            this.sumWeight = sumWeight;
-            this.maxWeight = maxWeight;
+    private boolean isApproachCapacity(int[] weights, int days, int capacity) {
+        int step = 0;
+        int curSum = 0;
+        for (int weight : weights) {
+            if (curSum + weight > capacity) {
+                step++;
+                curSum = weight;
+            } else {
+                curSum += weight;
+            }
         }
+        return step + 1 <= days;
     }
 }
