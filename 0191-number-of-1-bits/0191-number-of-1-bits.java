9bba39b70b862c73ef04f93c1492b8f127026a9e
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-11-29 11:19:59 +0300

    Time: 1 ms (13.13%), Space: 39.8 MB (21.66%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,31 @@
+
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution main = new Solution();
+        main.start();
+    }
+
+    private void start() {
+        test();
+    }
+
+    private void test() {
+        System.out.println(hammingWeight(1));
+    }
+
+    public int hammingWeight(int n) {
+        final String s = Integer.toBinaryString(n);
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                result++;
+            }
+        }
+        return result;
+    }
+
+
+}
