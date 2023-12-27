71b2fe29528805845712d1b5772c23ea56740214
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-12-27 23:10:04 +0300

    Time: 10 ms (42.50%), Space: 63 MB (5.00%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -0,0 +1,41 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution main = new Solution();
+        main.start();
+    }
+
+    private void start() {
+//        test();
+//        test1();
+        test2();
+    }
+
+    private void test() {
+        System.out.println("ожидаемый ответ 3; ф-" + minCost("abaac", new int[]{1, 2, 3, 4, 5}));
+    }
+
+    private void test1() {
+        System.out.println("ожидаемый ответ 0; ф-" + minCost("abc", new int[]{1, 2, 3}));
+    }
+
+    private void test2() {
+        System.out.println("ожидаемый ответ 2; ф-" + minCost("aabaa", new int[]{1, 2, 3, 4, 1}));
+    }
+
+    public int minCost(String colors, int[] neededTime) {
+        var result = 0;
+        final int[] restNeededTime = new int[neededTime.length + 1];
+        for (int i = 0; i < colors.length() - 1; i++) {
+            char color = colors.charAt(i);
+            if (color == colors.charAt(i + 1)) {
+                int curPrice = Math.max(restNeededTime[i], neededTime[i]);
+
+                int nextPrice = Math.max(curPrice, neededTime[i + 1]);
+                result += Math.min(curPrice, neededTime[i + 1]);
+                restNeededTime[i + 1] = nextPrice;
+            }
+        }
+        return result;
+    }
+}
