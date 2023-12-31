eaeec0c3ca769146d27b766597a477517d6195d6
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2023-12-31 04:32:49 +0300

    Time: 1 ms (86.22%), Space: 41.4 MB (13.28%) - LeetHub

---
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
@@ -0,0 +1,54 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution main = new Solution();
+        main.start();
+    }
+
+    private void start() {
+           test();
+        test1();
+        test2();
+    }
+
+    private void test() {
+        System.out.println("ожидаемый ответ 2; ф-" + maxLengthBetweenEqualCharacters("abca"));
+    }
+
+    private void test1() {
+        System.out.println("ожидаемый ответ -1; ф-" + maxLengthBetweenEqualCharacters("cbzxy"));
+    }
+
+
+    private void test2() {
+        System.out.println("ожидаемый ответ 0; ф-" + maxLengthBetweenEqualCharacters("aa"));
+    }
+
+
+    public int maxLengthBetweenEqualCharacters(String s) {
+        final String revers = new StringBuilder(s).reverse().toString();
+        final int length = s.length();
+        int left = 0;
+        int right = s.length() - 1;
+        int result = -1;
+        while (left < right) {
+            int lChar = s.charAt(left);
+            int rChar = s.charAt(right);
+            int lDif = length - revers.indexOf(lChar);
+            lDif = lDif - left-2;
+            lDif = getDif(lDif);
+            int rDif =s.indexOf(rChar);
+            rDif = right - rDif-2;
+            rDif = getDif(rDif);
+            int cur = Math.max(lDif, rDif);
+            result = Math.max(cur, result);
+            left++;
+            right--;
+        }
+        return result;
+    }
+
+    private int getDif(int dif) {
+        return dif < 0 ? -1 : dif;
+    }
+}
