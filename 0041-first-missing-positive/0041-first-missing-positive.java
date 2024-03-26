e1e8f5eeb080119c5cfab82d4fad21dae8a29d7b
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2024-03-26 14:53:57 +0300

    Time: 21 ms (6.45%), Space: 61.5 MB (5.17%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,22 +1,40 @@
-class Solution {
-     public int firstMissingPositive(int[] nums) {
-        int i = 0;
-        while (i < nums.length) {
-            int correct = nums[i] - 1;
-            if (nums[i] <= nums.length && nums[i] > 0 && nums[i] != nums[correct]) {
-                int temp = nums[i];
-                nums[i] = nums[correct];
-                nums[correct] = temp;
-            } else {
-                i++;
-            }
-        }
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != j + 1) {
-                return j + 1;
-            }
-        }
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
 
-        return nums.length + 1;
+class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.start();
+    }
+
+    private void start() {
+        test0();
+    }
+
+    private void test0() {
+        final int[] nums = {1, 3, 4, 2, 2, 3};
+        final var result = firstMissingPositive(nums);
+        System.out.println("ожидаемый   ответ - " + "5");
+        System.out.println("фактический ответ - " + result);
+        System.out.println("//////////////////////");
+    }
+
+    public int firstMissingPositive(int[] nums) {
+        final var positiveNumbers = new HashSet<Integer>();
+        for (var num : nums) {
+            if (num > 0 && !positiveNumbers.contains(num)) {
+                positiveNumbers.add(num);
+            }
+        }
+        final var sortedPositiveNumbers = new ArrayList<Integer>(positiveNumbers);
+        sortedPositiveNumbers.sort(Comparator.comparingInt(Integer::intValue));
+        for (int i = 0; i < sortedPositiveNumbers.size(); i++) {
+            if (sortedPositiveNumbers.get(i) != i + 1) {
+                return i + 1;
+            }
+        }
+        return sortedPositiveNumbers.size() + 1;
     }
 }
