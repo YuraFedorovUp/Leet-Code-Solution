f3e3253e2c69d4daff9197e2a42d3499bb506ed7
YuraFedorovUp <101806927+YuraFedorovUp@users.noreply.github.com>
2024-03-26 14:58:26 +0300

    Time: 1 ms (98.00%), Space: 55.2 MB (36.16%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -21,20 +21,24 @@ class Solution {
         System.out.println("//////////////////////");
     }
 
-    public int firstMissingPositive(int[] nums) {
-        final var positiveNumbers = new HashSet<Integer>();
-        for (var num : nums) {
-            if (num > 0) {
-                positiveNumbers.add(num);
+     public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] <= nums.length && nums[i] > 0 && nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            } else {
+                i++;
             }
         }
-        final var sortedPositiveNumbers = new ArrayList<Integer>(positiveNumbers);
-        sortedPositiveNumbers.sort(Comparator.comparingInt(Integer::intValue));
-        for (int i = 0; i < sortedPositiveNumbers.size(); i++) {
-            if (sortedPositiveNumbers.get(i) != i + 1) {
-                return i + 1;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                return j + 1;
             }
         }
-        return sortedPositiveNumbers.size() + 1;
+
+        return nums.length + 1;
     }
 }
